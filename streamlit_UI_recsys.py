339db6dcd7d45e620c1f01ac7eadcbdc81d45621
Jessica Echterhoff <j.m.echt@gmail.com>
2022-11-12 14:26:40 -0800

    update link

--- streamlit_UI_recsys.py
+++ streamlit_UI_recsys.py
@@ -142,6 +142,7 @@ def main():
             instances = movies.loc[st.session_state.selected_sequence].drop_duplicates(subset="imdbId")
 
             if 'film_info' not in st.session_state:
+                st.write(instances)
                 st.session_state['film_info'] = [get_film_info(instances=instances)]
     if st.session_state.state == 'review_items':
         instances = movies.loc[st.session_state.selected_sequence].drop_duplicates(subset="imdbId")
